77e62fa44c4ef3e0c03be3f0dc356303454802cc
Graham Arnett <GrahamKentArnett@outlook.com>
2024-10-25 16:54:59 -0400

    init commit

---
+++ Common Divisor Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Common_Divisor_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the common divisor app!");
+
+            String choice;
+            do
+            {
+                Console.Write("Enter first number: ");
+                int entryOne = int.Parse(Console.ReadLine());  
+                Console.Write("Enter second number: ");
+                int entryTwo = int.Parse(Console.ReadLine());
+
+                while (entryTwo != 0) 
+                {
+                    var oldEntryTwo = entryTwo;
+                    entryTwo = entryOne % entryTwo;
+                    entryOne = oldEntryTwo;
+                }
+                Console.WriteLine($"Greatest common divisor: {entryOne}");
+                Console.Write("Want to go again? (y/n): ");
+                choice = Console.ReadLine().ToLower();
+            } while (choice == "y");
+        }
+    }
+}
